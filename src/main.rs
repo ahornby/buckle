10e91ca92ea98569b76948dbb5c47626ad8d7af9
Alex Hornby <alex@hornby.org.uk>
2023-06-25 15:42:05 +0100

    toml config covering existing buck2 latest
    basic idea is that buckle downloads and caches archives,  and archives provide binaries

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Context, Error};
+use anyhow::{anyhow, bail, Context, Error};
 use serde::{Deserialize, Serialize};
 use std::{
     env,
@@ -6,15 +6,30 @@ use std::{
     path::{Path, PathBuf},
     process::{Command, Stdio},
 };
-use std::{io::Write, time::SystemTime};
+use std::{
+    ffi::{OsStr, OsString},
+    io::{self, Read, Write},
+    time::SystemTime,
+};
 use tempfile::NamedTempFile;
 use url::Url;
 
 #[cfg(unix)]
 use std::os::unix::fs::PermissionsExt;
 
+mod config;
+
+use config::{ArchiveConfig, BuckleConfig, BuckleSource, GithubRelease, PackageType};
+
+const BUCKLE_BINARY: &str = "BUCKLE_BINARY";
+const BUCKLE_CONFIG: &str = "BUCKLE_CONFIG";
+const BUCKLE_CONFIG_FILE: &str = "BUCKLE_CONFIG_FILE";
+const BUCKLE_SCRIPT: &str = "BUCKLE_SCRIPT";
+const BUCKLE_HOME: &str = "BUCKLE_HOME";
+const BUCKLE_REPO_CONFIG: &str = ".buckleconfig.toml";
+
 fn get_buckle_dir() -> Result<PathBuf, Error> {
-    let mut dir = match env::var("BUCKLE_HOME") {
+    let mut dir = match env::var(BUCKLE_HOME) {
         Ok(home) => Ok(PathBuf::from(home)),
         Err(_) => match env::consts::OS {
             "linux" => {
@@ -83,7 +98,7 @@ pub struct Release {
     pub assets: Vec<Asset>,
 }
 
-fn get_releases(path: &Path) -> Result<Vec<Release>, Error> {
+fn get_releases(gh_release: &GithubRelease, path: &Path) -> Result<Vec<Release>, Error> {
     let mut releases_json_path = path.to_path_buf();
     releases_json_path.push("releases.json");
 
@@ -103,8 +118,12 @@ fn get_releases(path: &Path) -> Result<Vec<Release>, Error> {
     let client = reqwest::blocking::Client::builder()
         .user_agent("buckle")
         .build()?;
+
     let releases = client
-        .get("http://api.github.com/repos/facebook/buck2/releases")
+        .get(format!(
+            "http://api.github.com/repos/{}/{}/releases",
+            gh_release.owner, gh_release.repo
+        ))
         .send()?;
     let text = releases.text_with_charset("utf-8")?;
     let mut file = File::create(releases_json_path)?;
@@ -113,7 +132,9 @@ fn get_releases(path: &Path) -> Result<Vec<Release>, Error> {
     Ok(serde_json::from_str(&text)?)
 }
 
-fn get_arch() -> Result<&'static str, Error> {
+// Approximate the target triple for the current platform
+// as per https://rust-lang.github.io/rfcs/0131-target-specification.html
+fn get_target() -> Result<&'static str, Error> {
     Ok(match env::consts::ARCH {
         "x86_64" => match env::consts::OS {
             "linux" => "x86_64-unknown-linux-gnu",
@@ -130,22 +151,44 @@ fn get_arch() -> Result<&'static str, Error> {
     })
 }
 
-fn download_http(version: String, output_dir: &Path) -> Result<PathBuf, Error> {
-    let releases = get_releases(output_dir)?;
-    let mut path = output_dir.to_path_buf();
+fn get_triple_os() -> &'static str {
+    match env::consts::OS {
+        "linux" => "linux",
+        "macos" => "darwin",
+        "windows" => "windows",
+        os => panic!("Unsupported OS {}", os),
+    }
+}
 
+fn download_from_github(
+    artifact_pattern: &str,
+    gh_release: &GithubRelease,
+    output_dir: &Path,
+) -> Result<(PathBuf, reqwest::blocking::Response), Error> {
+    let releases = get_releases(gh_release, output_dir)?;
+
+    let mut path = output_dir.to_path_buf();
     let mut artefact = None;
-    let arch = get_arch()?;
+
+    // simple templating, we'll need this even if we add regex support
+    let mut artifact_name = artifact_pattern.to_string();
+    artifact_name = artifact_name.replace("%version%", &gh_release.version);
+    artifact_name = artifact_name.replace("%arch%", env::consts::ARCH);
+    artifact_name = artifact_name.replace("%os%", get_triple_os());
+    artifact_name = artifact_name.replace("%target%", get_target()?);
 
     for release in releases {
-        if release.name.as_ref() == Some(&version) {
+        if release.name.as_ref() == Some(&gh_release.version) {
             if release.tag_name == "latest" {
                 path.push(release.target_commitish);
+            } else {
+                // TODO use sha256 checksum if present
+                path.push(release.name.ok_or_else(|| anyhow!("Release has no name"))?);
             }
             for asset in release.assets {
                 let name = asset.name;
                 let url = asset.browser_download_url;
-                if name == format!("buck2-{}.zst", arch) {
+                if name == artifact_name {
                     artefact = Some((name, url));
                     break;
                 }
@@ -156,81 +199,179 @@ fn download_http(version: String, output_dir: &Path) -> Result<PathBuf, Error> {
     let (_name, url) = if let Some((name, url)) = artefact {
         (name, url)
     } else {
-        return Err(anyhow!(
-            "Meta has changed their releasing method. Please update buckle."
-        ));
+        bail!(
+            "Could not find {}. Its possible {} has changed their releasing method for {}. Please update buckle.",
+            artifact_name,
+            gh_release.owner,
+            gh_release.repo
+        )
     };
 
-    let final_name = {
-        let mut p = path.clone();
-        p.push("buck2");
-        p
-    };
+    if !path.exists() {
+        fs::create_dir_all(&path).with_context(|| anyhow!("error creating {:?}", path))?;
+    }
+
+    Ok((path, reqwest::blocking::get(url)?))
+}
+
+fn extract<R>(
+    unpacked_name: &str,
+    package_type: &PackageType,
+    mut archive_stream: R,
+    path: PathBuf,
+) -> Result<PathBuf, Error>
+where
+    R: Read,
+{
+    let mut final_name = path.clone();
+    final_name.push(unpacked_name);
     if final_name.exists() {
         // unpacked already present, so do nothing
         return Ok(final_name);
     }
 
-    fs::create_dir_all(&path).with_context(|| anyhow!("problem creating {:?}", path))?;
-
     // create a temporary file in the same directory as the final file
-    let mut tmp_file = NamedTempFile::new_in(&path)?;
+    let mut tmp_file = NamedTempFile::new_in(path)?;
 
-    let resp = reqwest::blocking::get(url)?;
-    zstd::stream::copy_decode(resp, &tmp_file)?;
+    match package_type {
+        PackageType::SingleFile => {
+            io::copy(&mut archive_stream, &mut tmp_file)
+                .with_context(|| anyhow!("problem copying to tmp_file"))?;
+        }
+        PackageType::ZstdSingleFile => zstd::stream::copy_decode(archive_stream, &tmp_file)?,
+    }
+
     tmp_file.flush()?;
+
     let permissions = fs::Permissions::from_mode(0o755);
     fs::set_permissions(tmp_file.path(), permissions)?;
 
     // only move to final name once fully written and stable
     fs::rename(tmp_file.path(), &final_name)
         .with_context(|| anyhow!("problem renaming to {:?}", final_name))?;
-
-    Ok(path)
+    Ok(final_name)
 }
 
-fn read_buck2_version() -> Result<String, Error> {
-    if let Ok(version) = env::var("USE_BUCK2_VERSION") {
-        return Ok(version);
-    }
-
-    if let Some(mut root) = find_project_root()? {
-        root.push(".buckversion");
-        if root.exists() {
-            return Ok(fs::read_to_string(root)?);
+fn download(
+    binary_name: &str,
+    archive_config: &ArchiveConfig,
+    output_dir: &Path,
+) -> Result<PathBuf, Error> {
+    let (path, stream) = match &archive_config.source {
+        BuckleSource::Github(ref gh_release) => {
+            download_from_github(&archive_config.artifact_pattern, gh_release, output_dir)?
         }
-    }
-
-    Ok(String::from("latest"))
+    };
+    extract(binary_name, &archive_config.package_type, stream, path)
 }
 
-fn get_buck2_path() -> Result<PathBuf, Error> {
-    let buck2_dir = get_buckle_dir()?.join("buck2");
+fn get_binary_path(config: BuckleConfig, binary_name: Option<&String>) -> Result<PathBuf, Error> {
+    let binary_name = if let Some(binary_name) = binary_name {
+        binary_name
+    } else if config.binaries.len() == 1 {
+        // only one so default to it
+        config.binaries.keys().next().unwrap()
+    } else {
+        bail!("No binary name provided");
+    };
 
-    if !buck2_dir.exists() {
-        fs::create_dir_all(&buck2_dir)?;
+    let bin_config = config
+        .binaries
+        .get(binary_name)
+        .ok_or_else(|| anyhow!("No binary named {} in buckle config", binary_name))?;
+    let archive_name = &bin_config.provided_by;
+    let archive_config = config
+        .archives
+        .get(archive_name)
+        .ok_or_else(|| anyhow!("No archive named {} in buckle config", archive_name))?;
+
+    let archive_dir = get_buckle_dir()?.join(archive_name);
+
+    if !archive_dir.exists() {
+        fs::create_dir_all(&archive_dir)
+            .with_context(|| anyhow!("error creating {:?}", archive_dir))?;
     }
 
-    let buck2_version = read_buck2_version()?;
+    download(binary_name, archive_config, &archive_dir).map_err(Error::into)
+}
 
-    // or any version, but that isn't supported yet
-    // TODO probably a regex here.
-    if buck2_version == "latest" {
-        Ok(download_http(buck2_version, &buck2_dir)?)
+fn load_config(args: &mut env::ArgsOs) -> Result<BuckleConfig, Error> {
+    if let Ok(config) = env::var(BUCKLE_CONFIG) {
+        // Short circuit if the user has given us a config in the environment
+        return Ok(toml::from_str(&config)?);
+    }
+
+    let config_file = if env::var(BUCKLE_SCRIPT).is_ok() {
+        Some(PathBuf::from(args.next().unwrap().to_str().unwrap()))
     } else {
-        // TODO fetch from git.
-        todo!()
+        match env::var(BUCKLE_CONFIG_FILE) {
+            Ok(file) => Some(PathBuf::from(file)),
+            // Then try repo level config
+            Err(_) => {
+                if let Some(mut root) = find_project_root()? {
+                    root.push(BUCKLE_REPO_CONFIG);
+                    if root.exists() {
+                        Some(root)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            }
+        }
+    };
+
+    if let Some(config_file) = config_file {
+        let config = &fs::read_to_string(&config_file).with_context(|| {
+            format!(
+                "Could not read config file {:?}",
+                config_file.to_string_lossy()
+            )
+        })?;
+        let config = toml::from_str(config)?;
+        Ok(config)
+    } else {
+        //dbg!("No config file found, using builtin buck2 defaults");
+        Ok(BuckleConfig::buck2_latest())
+    }
+}
+
+// What binary are we trying to run from the buckle config?
+fn get_binary_name(invoked_as: Option<&OsString>) -> Option<String> {
+    if let Ok(binary_name) = env::var(BUCKLE_BINARY) {
+        Some(binary_name)
+    } else if let Some(invoked_as) = invoked_as {
+        let base_name = Path::new(&invoked_as).file_name();
+        if base_name == Some(OsStr::new("buckle")) {
+            None
+        } else {
+            base_name.and_then(|v| v.to_str()).map(|v| v.to_string())
+        }
+    } else {
+        None
     }
 }
 
 fn main() -> Result<(), Error> {
-    let binary_path = dbg!(get_buck2_path()?);
+    // Collect information intended for invoked binary.
 
     // Collect information indented for buck2 binary.
     let mut args = env::args_os();
-    args.next(); // Skip buckle
+    // Figure out what binary we are trying to run.
+    let invoked_as = args.next();
+    let binary_name = get_binary_name(invoked_as.as_ref());
+    let binary_name = binary_name;
+    let buckle_config = load_config(&mut args)?;
+    let binary_path = get_binary_path(buckle_config, binary_name.as_ref())?;
 
-    let envs = env::vars_os();    
+    if env::var(BUCKLE_SCRIPT).is_err() {
+        eprintln!("buckle is running {:?}", binary_path)
+    }
+
+    // Remove so any recursive buckle calls need their own #! to be in script mode
+    env::remove_var(BUCKLE_SCRIPT);
+    let envs = env::vars_os();
 
     // Pass all file descriptors through as well.
     let status = Command::new(&binary_path)
